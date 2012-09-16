c3f69c3f2e42a0d6025acf469cdd0d52cc291999
Thomas Thurman <thomas@thurman.org.uk>
2012-09-16 01:09:33 +0100

    Basic main program to test aptfile.c; should not be shipped in the final version.

---
+++ aptfile_tester.c
@@ -0,0 +1,31 @@
+/*
+ Testing mechanism for aptfile.c; not to be shipped in the final version.
+
+gcc aptfile_tester.c aptfile.c $(pkg-config glib-2.0 --cflags --libs) -o aptfile_tester
+
+*/
+
+#include "config.h"
+#include "aptfile.h"
+
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+	gchar *result = NULL;
+
+	if (argc<2) {
+		printf ("Give the package name.\n");
+		return 1;
+	}
+
+	printf ("Looking up: %s\n", argv[1]);
+	
+	result = aptfile_owner_package (argv[1]);
+
+	printf ("Result is: %s\n", result);
+
+	return 0;
+}
+
